8df306641e61a95278877048d1ec19b8882d515f
Zeinab Ismael <153861416+ZeinabIsmael9@users.noreply.github.com>
2024-10-07 06:26:48 -0700

    MVC installations

---
+++ 07-MVC/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mvc/elframework',
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mvc/elframework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zeinabismael/elfarmework' => array(
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'a9f7370a8ea894ee80515749ebc96c47365a05c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../zeinabismael/elfarmework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
